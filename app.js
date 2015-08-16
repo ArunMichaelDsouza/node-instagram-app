de60e485c220b46260a0e092269fa9bf821d8a46
Arun Michael Dsouza <amdsouza92@gmail.com>
2015-08-17 01:05:21 +0530

    Setting templating engine as ejs and adding view files

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 // Requiring express module
 var express = require('express');
+// Requiring instagram module
+var ig = require('instagram-node').instagram();
 
 // Initiating express app
 var app = express();
 
 // Setting root folder to look for static files
 app.use(express.static(__dirname+'/www/'));
+// Setting view engine to ejs
+app.set('view engine', 'ejs');
 
 // Serving static file on default route get
 app.get('/', function(req, res) {

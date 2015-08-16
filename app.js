46de0c667ab52fde49162a4c599515c84387dd86
Arun Michael Dsouza <amdsouza92@gmail.com>
2015-08-16 23:46:52 +0530

    Creating http server

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+// Requiring http module
 var http = require('http');
 
+// Creating http server
 http.createServer(function(req, res) {
-	res.writeHead(200);
-	res.write('Hello World!');
-	res.end();
-}).listen(8000);
+	res.writeHead(200); // Setting header response code
+	res.write('Hello World!'); // Writing to body
+	res.end(); // Ending response
+}).listen(8000); // Listening at port
 
-console.log('Server Running...');
+console.log('Server Running...'); // Logging to server console

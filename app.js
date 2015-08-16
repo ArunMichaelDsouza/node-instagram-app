5312de97218dc844fbc5b0b62c0ccd5fdaf05e2a
Arun Michael Dsouza <amdsouza92@gmail.com>
2015-08-17 00:37:56 +0530

    Default static file lookup setting for express

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ var express = require('express');
 // Initiating express app
 var app = express();
 
+// Setting root folder to look for static files
+app.use(express.static(__dirname+'/www/'));
+
 // Serving static file on default route get
 app.get('/', function(req, res) {
-	res.sendFile(__dirname+'/www/index.html');
+	res.sendFile('index.html');
 }).listen(8000);
 
 console.log('Server Running...');

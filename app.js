14eacc909906064e13aff4758417ede935f8b60c
Arun Michael Dsouza <amdsouza92@gmail.com>
2015-08-18 01:06:41 +0530

    Adding instagram app credentials

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ app.use(express.static(__dirname+'/assets'));
 // Setting view engine to ejs
 app.set('view engine', 'ejs');
 
+// Configuring instagram app credentials
+ig.use({
+	client_id: 'e9b7c83b11ca4b8c8486aa02e7363934',
+	client_secret : '9d60e4b6aece40d18b369554912b9aac'
+});
+
 // Serving static file on default route get
 app.get('/', function(req, res) {
 	res.render('pages/index');
